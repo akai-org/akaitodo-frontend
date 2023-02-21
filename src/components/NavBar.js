6b6c88b7f52166247f131d03691ab946b0f6d792
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2023-02-21 01:52:18 +0100

    navbar main build

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/App.scss";
 import "../styles/NavBar.scss";
@@ -7,69 +7,87 @@ function NavBar() {
   const [click, setClick] = useState(false);
 
   const handleclick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const closeMobileMenu = () => {
+    if (window.innerWidth <= 900)
+      setClick(true);
+  }
+
+  const mobileMenu = () => {
+    if (window.innerWidth <= 1024) {
+      setClick(true);
+    }
+    else {
+      setClick(false);
+    }
+  }
+
+  window.addEventListener('resize', mobileMenu);
+
+  useEffect(() => {
+    mobileMenu();
+  }, []);
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
+      <div className={click ? 'navbar-container hide' : 'navbar-container'}>
 
-        <div className="upper-container">
-          <div className="logo-container">
-            <Link to="/" className="logo-image-link">
-            <div className="logo-image"> <i className="ri-checkbox-blank-circle-line"></i> </div>
+        <div className={click ? 'upper-container hide' : 'upper-container'}>
+          <div className={click ? 'logo-container hide' : 'logo-container'}>
+            <Link to="/" className={click ? 'logo-image-link hide' : 'logo-image-link'}>
+            <div className={click ? 'logo-image hide' : 'logo-image'}> <i className="ri-checkbox-blank-circle-line"></i> </div>
             </Link>
             <Link to="/">
-            <div className="logo-app-name">DoDo</div>
+            <div className={click ? 'logo-app-name hide' : 'logo-app-name'}>DoDo</div>
             </Link>
           </div>
 
-          <div className="menu-icon" onClick={handleclick}>
+          <div className={click ? 'menu-icon hide' : 'menu-icon'} onClick={handleclick}>
             <i className={click ? 'ri-menu-fill' : 'ri-close-fill'}></i>
           </div>
         </div>
 
-        <div className="menu-container">
-          <ul className={click ? 'menu-list active' : 'menu-list'}>
-            <li className="menu-item">
-              <Link to="/" className="menu-link" onClick={closeMobileMenu}>
-                <div className="menu-link-icon"><i className="ri-home-line"></i></div>
-                <div className="menu-link-tag">Home</div>
+        <div className={click ? 'menu-container hide' : 'menu-container'}>
+          <ul className={click ? 'menu-list hide' : 'menu-list'}>
+            <li className={click ? 'menu-item hide' : 'menu-item'}>
+              <Link to="/" className={click ? 'menu-link hide' : 'menu-link'} onClick={closeMobileMenu}>
+                <div className={click ? 'menu-link-icon hide' : 'menu-link-icon'}><i className="ri-home-line"></i></div>
+                <div className={click ? 'menu-link-tag hide' : 'menu-link-tag'}>Home</div>
               </Link>
             </li>
-            <li className="menu-item">
-              <Link to="/calendar" className="menu-link" onClick={closeMobileMenu}>
-              <div className="menu-link-icon"><i className="ri-calendar-line"></i></div>
-              <div className="menu-link-tag">Calendar</div>
+            <li className={click ? 'menu-item hide' : 'menu-item'}>
+              <Link to="/calendar" className={click ? 'menu-link hide' : 'menu-link'} onClick={closeMobileMenu}>
+              <div className={click ? 'menu-link-icon hide' : 'menu-link-icon'}><i className="ri-calendar-line"></i></div>
+              <div className={click ? 'menu-link-tag hide' : 'menu-link-tag'}>Calendar</div>
               </Link>
             </li>
-            <li className="menu-item">
-              <Link to="/todolist" className="menu-link" onClick={closeMobileMenu}>
-              <div className="menu-link-icon"><i className="ri-file-list-2-line"></i></div>
-              <div className="menu-link-tag">Todo list</div>
+            <li className={click ? 'menu-item hide' : 'menu-item'}>
+              <Link to="/todolist" className={click ? 'menu-link hide' : 'menu-link'} onClick={closeMobileMenu}>
+              <div className={click ? 'menu-link-icon hide' : 'menu-link-icon'}><i className="ri-file-list-2-line"></i></div>
+              <div className={click ? 'menu-link-tag hide' : 'menu-link-tag'}>Todo list</div>
               </Link>
             </li>
-            <li className="menu-item">
-              <Link to="/categories" className="menu-link" onClick={closeMobileMenu}>
-              <div className="menu-link-icon"><i className="ri-bar-chart-horizontal-line"></i></div>
-              <div className="menu-link-tag">Categories</div>
+            <li className={click ? 'menu-item hide' : 'menu-item'}>
+              <Link to="/categories" className={click ? 'menu-link hide' : 'menu-link'} onClick={closeMobileMenu}>
+              <div className={click ? 'menu-link-icon hide' : 'menu-link-icon'}><i className="ri-bar-chart-horizontal-line"></i></div>
+              <div className={click ? 'menu-link-tag hide' : 'menu-link-tag'}>Categories</div>
               </Link>
             </li>
-            <li className="menu-item">
-              <Link to="/notes" className="menu-link" onClick={closeMobileMenu}>
-              <div className="menu-link-icon"><i className="ri-booklet-line"></i></div>
-              <div className="menu-link-tag">Notes</div>
+            <li className={click ? 'menu-item hide' : 'menu-item'}>
+              <Link to="/notes" className={click ? 'menu-link hide' : 'menu-link'} onClick={closeMobileMenu}>
+              <div className={click ? 'menu-link-icon hide' : 'menu-link-icon'}><i className="ri-booklet-line"></i></div>
+              <div className={click ? 'menu-link-tag hide' : 'menu-link-tag'}>Notes</div>
               </Link>
             </li>
-            <li className="menu-item">
-              <Link to="/" className="menu-link" onClick={closeMobileMenu}>
-              <div className="menu-link-icon"><i className="ri-settings-4-line"></i></div>
-              <div className="menu-link-tag">Settings</div>
+            <li className={click ? 'menu-item hide' : 'menu-item'}>
+              <Link to="/" className={click ? 'menu-link hide' : 'menu-link'} onClick={closeMobileMenu}>
+              <div className={click ? 'menu-link-icon hide' : 'menu-link-icon'}><i className="ri-settings-4-line"></i></div>
+              <div className={click ? 'menu-link-tag hide' : 'menu-link-tag'}>Settings</div>
               </Link>
             </li>
           </ul>
         </div>
 
-        <div className="user-container">
+        <div className={click ? 'user-container hide' : 'user-container'}>
           <div className="user-tile">
             <div className="user-avatar"></div>
             <div className="user-name"></div>
