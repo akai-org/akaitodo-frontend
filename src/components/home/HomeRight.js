fb5add8dad9f1d23d86827d610db24b3a853f149
Aeri4a <ankaxgry@gmail.com>
2023-04-27 16:53:06 +0200

    modules & folders

---
+++ src/components/home/HomeRight.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../styles/App.scss";
+import styles from "../../styles/components/home/HomeRight.module.scss";
+
+function HomeRight() {
+  return <div className={styles.container}>right div</div>;
+}
+
+export default HomeRight;
---
+++ src/components/widgets/CalendarWidget.js
@@ -0,0 +1,23 @@
+import CalendarWidgetAddon from "./CalendarWidgetAddon";
+import styles from "../../styles/components/widgets/CalendarWidget.module.scss";
+
+function CalendarWidget() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.date}>
+          <span className={styles.dateSpan}> Monday, 12th January 2022</span>
+        </div>
+
+        <div className={styles.specifiers}>
+          <CalendarWidgetAddon id="1">Day</CalendarWidgetAddon>
+          <CalendarWidgetAddon id="2">Month</CalendarWidgetAddon>
+          <CalendarWidgetAddon id="3">Year</CalendarWidgetAddon>
+        </div>
+        <div className={styles.block}>infs</div>
+      </div>
+    </>
+  );
+}
+
+export default CalendarWidget;
