24b6bf058d3eb8923d9fca01c301e205535c4ee7
Olej <music.olejniczak@wp.pl>
2023-04-27 16:48:50 +0200

    Notes start, no modules yet

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import Notes from "./pages/Notes";
 import "./styles/App.scss";
 
 function App() {
   return (
     <main className="main" themestyle="default" thememode="light">
       <Router>
-        <NavBar/>
+        <NavBar />
         <div className="main-container">
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/calendar" element={<Home/>}/>
-            <Route path="/todolist" element={<Home/>}/>
-            <Route path="/categories" element={<Home/>}/>
-            <Route path="/notes" element={<Home/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/calendar" element={<Home />} />
+            <Route path="/todolist" element={<Home />} />
+            <Route path="/categories" element={<Home />} />
+            <Route path="/notes" element={<Notes />} />
           </Routes>
         </div>
       </Router>
