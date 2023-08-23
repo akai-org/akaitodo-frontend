f82dae1ee245ff2b0dba2adc002c357b0fc4d9ac
giro39 <127617431+giro39@users.noreply.github.com>
2023-08-23 21:52:59 +0200

    Part 1.1 partially done, added basic ui

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,7 +3,21 @@ import "../styles/App.scss";
 import styles from "../styles/pages/Settings.module.scss";
 
 function Settings() {
-  return <div>Settings</div>;
+  return (
+  <div className={styles.container}>
+    <div className={styles.line}>
+      <div className={styles.themeDiv}>
+        <h3>THEME</h3>
+        <button className={styles.themeButton}></button>
+      </div>
+      <div className={styles.modeDiv}>
+        <h3>MODE</h3>
+        <button className={styles.themeButton}></button>
+      </div>
+    </div>
+    <hr></hr>
+  </div>
+  )
 }
 
 export default Settings;
