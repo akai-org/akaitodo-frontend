c658b56c1de0b3a87b232c05ed5e543018983ea2
giro39 <127617431+giro39@users.noreply.github.com>
2023-08-24 21:52:27 +0200

    first lines into the colour choice component

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,22 +2,46 @@ import React from "react";
 import "../styles/App.scss";
 import styles from "../styles/pages/Settings.module.scss";
 
-function Settings() {
-  return (
-  <div className={styles.container}>
-    <div className={styles.line}>
-      <div className={styles.themeDiv}>
-        <h3>THEME</h3>
-        <button className={styles.themeButton}></button>
+const Settings = () => (
+  <>
+    <div className={styles.container}>
+      <div className={styles.line}>
+        <div className={styles.themeDiv}>
+          <h3>THEME</h3>
+          <button className={styles.themeButton}></button>
+        </div>
+        <div className={styles.modeDiv}>
+          <h3>MODE</h3>
+          <div className={styles.modeSwitch}>
+            <input type="checkbox"></input>
+            <div className={styles.modeSlider}></div>
+          </div>
+        </div>
       </div>
-      <div className={styles.modeDiv}>
-        <h3>MODE</h3>
-        <button className={styles.themeButton}></button>
+      <hr></hr>
+    </div>
+    <ColorChoice />
+  </>
+)
+
+
+
+const ColorChoice = () => (
+  <>
+    <div className={styles.cContainer}>
+      <div className={styles.cColor}>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
       </div>
     </div>
-    <hr></hr>
-  </div>
-  )
-}
+  </>
+)
 
 export default Settings;
