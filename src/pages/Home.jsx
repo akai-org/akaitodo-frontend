791a758a50952433cb36e4c991bc2adade8b11cb
Anna <80831299+Aeri4a@users.noreply.github.com>
2023-10-10 00:59:46 +0200

    Migration to Vite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/App.scss';
 
 import HomeMiddle from '../components/home/HomeMiddle';
-import HomeRight from '../components//home/HomeRight';
+import HomeRight from '../components/home/HomeRight';
 import styles from '../styles/pages/Home.module.scss';
 
 const Home = () => {
