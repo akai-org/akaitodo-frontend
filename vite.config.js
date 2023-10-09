99739208a0e0126502e0b91dac894a734809cf59
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2023-10-10 00:57:41 +0200

    migration to Vite

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+    return {
+        build: {
+            outDir: 'build',
+        },
+        plugins: [
+            // {
+            //     name: 'treat-js-files-as-jsx',
+            //     async transform(code, id) {
+            //         if (!id.match(/src\/.*\.js$/)) return null;
+
+            //         // Use the exposed transform from vite, instead of directly
+            //         // transforming with esbuild
+            //         return transformWithEsbuild(code, id, {
+            //             loader: {
+            //                 '.js': 'jsx',
+            //             },
+            //             jsx: 'automatic',
+            //         });
+            //     },
+            // },
+            react(),
+        ],
+    };
+});
