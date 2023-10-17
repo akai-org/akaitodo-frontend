487f59c92b351c0f9c4667ac7617084269c5152c
Olej <music.olejniczak@wp.pl>
2023-10-17 19:03:01 +0200

    Redux implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.scss';
 
@@ -6,25 +7,28 @@ import Navbar from './layout/Navbar/Navbar';
 import ScrollableContainer from './layout/ScrollableContainer/ScrollableContainer';
 import Home from './pages/Home';
 import Settings from './pages/Settings';
+import store from './store';
 
 const App = () => {
     return (
         <main className="Main" themeStyle="default" themeMode="light">
             {/* login logic here */}
-            <Router>
-                <Navbar />
-                <ScrollableContainer>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/calendar" element={<Home />} />
-                        <Route path="/todolist" element={<Home />} />
-                        <Route path="/categories" element={<Home />} />
-                        <Route path="/notes" element={<Home />} />
-                        <Route path="/settings" element={<Settings />} />
-                    </Routes>
-                </ScrollableContainer>
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <Navbar />
+                    <ScrollableContainer>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/calendar" element={<Home />} />
+                            <Route path="/todolist" element={<Home />} />
+                            <Route path="/categories" element={<Home />} />
+                            <Route path="/notes" element={<Home />} />
+                            <Route path="/settings" element={<Settings />} />
+                        </Routes>
+                    </ScrollableContainer>
+                </Router>
+            </Provider>
         </main>
     );
 };
---
+++ src/store/slices/Auth/Auth.extraReducers.js
@@ -0,0 +1,8 @@
+const buildExtraReducers = (builder) => {
+    // // thunks (pending, full-field, rejected)
+    // builder.addCase(getAllWizards.fulfilled, (state, { payload }) => {
+    //     state.wizards = payload;
+    // });
+};
+
+export default buildExtraReducers;
