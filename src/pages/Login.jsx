08c0b85caf5ab991f2e65f7b2936658917bb2b51
Anna <80831299+Aeri4a@users.noreply.github.com>
2023-10-25 19:53:32 +0200

    Added login page (#34)
    * Add login component, add form structure
    
    * Fix login page display
    
    * Add login component with styling
    
    To do:
    bottom block,
    media queries for non-pc users
    
    * Recreate Figma design
    
    todo:
    media queries,
    transform inputs.
    
    * Add google icon, fix padding in the google button
    
    * Fix inputs (comment z-indexes)
    
    * login validation done
    
    * Fixed the layers
    
    ---------
    
    Co-authored-by: giro39 <jan.wysmolinski@student.put.poznan.pl>

---
+++ src/pages/Login.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/pages/Login.module.scss';
+
+const initialFormState = {
+    login: '',
+    password: '',
+};
+
+const Login = () => {
+    const [form, setForm] = useState(initialFormState);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleLoginChange = (event) => {
+        const login = event.target.value;
+        setForm((prevState) => ({ ...prevState, login }));
+    };
+
+    const handlePasswordChange = (event) => {
+        const password = event.target.value;
+        setForm((prevState) => ({ ...prevState, password }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormErrors(validation(form));
+        setIsSubmit(true);
+    }
+
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(form); // when everything is fine
+        }
+    },[formErrors]);
+
+    const validation = (form) => {
+        const errors = {};
+        const emailRegex = /^[\w-\.]{1,30}@([\w-]+\.)+[\w-]{2,4}$/g;
+        if (!form.login) {
+            errors.login = "Login is required";
+        } else if (!emailRegex.test(form.login)) {
+            errors.login = "This login is not a valid e-mail format";
+        }
+        if (!form.password) {
+            errors.password = "Password is required";
+        }
+
+        return errors;
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.loginContainer}>
+                <div className={styles.leftSide}>
+                    <div className={styles.topWelcome}>
+                        <h1>Welcome!</h1>
+                        <img src="/favicon.ico" alt="DoDo Logo" />
+                    </div>
+                    <p className={styles.leftSideFirstSentence}>This is <span style={{ color: "#4DAEC3" }}>DoDo</span></p>
+                    <p>Your new bird friend in organizing things and time.</p>
+                    <p className={styles.leftSideLastSentence}>We hope you will enjoy the app.</p>
+                </div>
+
+                <button className={styles.middleBar}></button>
+
+                <div className={styles.rightSide}>
+                    <button className={styles.googleButton}>
+                        <img src="/images/Google__G__Logo.png" alt="" /> 
+                        Continue with&nbsp;<span className={styles.googleAccountBold}>Google Account</span>
+                    </button>
+                    <p>or</p>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={form.login}
+                            onChange={handleLoginChange}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={form.password}
+                            onChange={handlePasswordChange}
+                        />
+                        <button type="submit" className={styles.loginButton}>Login</button>
+                    </form>
+                </div>
+
+                <div className={styles.bottomBlock}></div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
