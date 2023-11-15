5b1e7aee072bbe8bc1f7dfc068c6b5cb5eef3092
kasiarog <kasiarog2002@gmail.com>
2023-11-15 16:52:58 +0100

    small fixes + props

--- src/components/widgets/ModalWidget.jsx
+++ src/components/widgets/ModalWidget.jsx
@@ -3,16 +3,16 @@ import React, { useState } from 'react';
 import styles from '../../styles/components/widgets/ModalWidget.module.scss';
 
 
-const ModalWidget = () => {
+const ModalWidget = ({header, paragraph}) => {
     return (
         <div className={styles.modalBackground}>
             <div className={styles.modalContainer}>
 
                 <div className={styles.header}>
-                    <h2 className={styles.headerText}>HEADER</h2>
+                    <h2 className={styles.headerText}>{header}</h2>
                 </div>
                 <div className={styles.paragraph}>
-                    <p>--------------------Paragraph--------------------</p>
+                    <p>{paragraph}</p>
                 </div>
                 <div className={styles.footer}>
                     <button className={`${styles.modalButton} ${styles.cancel}`}>Cancel</button>
