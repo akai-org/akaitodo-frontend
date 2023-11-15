6d1388c400ad84954d996f468466c530187cba86
Jan Wysmoliński <127617431+giro39@users.noreply.github.com>
2023-11-15 20:10:45 +0100

    Login page corrections (#38)
    * Add immunity to page scaling
    
    * Add >768px width handling
    
    * Add adjustments before mobile
    
    * Rewrite Login.module.scss with new media queries
    
    * Add first sketch of mobile design
    
    * Add mobile design
    
    Some phones are still too long, but it will be done
    
    * Fix some little things (unimportant)
    
    * Update h1Welcome class name
    
    * Ania  wants something

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -54,30 +54,30 @@ const Login = () => {
             <div className={styles.loginContainer}>
                 <div className={styles.leftSide}>
                     <div className={styles.topWelcome}>
-                        <h1>Welcome!</h1>
-                        <img src="/favicon.ico" alt="DoDo Logo" />
+                        <h1 className={styles.welcomeHeader}>Welcome!</h1>
+                        <img src="/favicon.ico" alt="DoDo Logo" className={styles.dodoLogo}/>
                     </div>
                     <p className={styles.leftSideFirstSentence}>
-                        This is <span style={{ color: '#4DAEC3' }}>DoDo</span>
+                        This is <span className={styles.dodoName}>DoDo</span>
                     </p>
-                    <p>Your new bird friend in organizing things and time.</p>
+                    <p className={styles.leftSideDescription}>Your new bird friend in&nbsp;organizing things and time.</p>
                     <p className={styles.leftSideLastSentence}>
                         We hope you will enjoy the app.
                     </p>
                 </div>
 
-                <button className={styles.middleBar}></button>
+                <div className={styles.middleBar}></div>
 
                 <div className={styles.rightSide}>
                     <button className={styles.googleButton}>
-                        <img src="/images/Google__G__Logo.png" alt="" />
+                        <img src="/images/Google__G__Logo.png" alt="" className={styles.googleLogo}/>
                         Continue with&nbsp;
                         <span className={styles.googleAccountBold}>
                             Google Account
                         </span>
                     </button>
-                    <p>or</p>
-                    <form onSubmit={handleSubmit}>
+                    <p className={styles.orText}>or</p>
+                    <form className={styles.loginForm} onSubmit={handleSubmit}>
                         <input
                             type="text"
                             placeholder="Username"
@@ -95,7 +95,10 @@ const Login = () => {
                         </button>
                     </form>
                 </div>
-
+                <div className={styles.topMobile}>
+                        <p className={styles.dodoNameMobile}>DoDo</p>
+                        <hr className={styles.hrLineMobile}></hr>
+                </div>
                 <div className={styles.bottomBlock}></div>
             </div>
         </div>
