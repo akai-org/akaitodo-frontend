eee8ec3ad761e51a03b3d6fef0800f189d9b9d3c
giro39 <127617431+giro39@users.noreply.github.com>
2024-01-03 19:47:47 +0100

    Add settings component

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -3,7 +3,23 @@ import '#src/styles/App.scss';
 import styles from '#src/styles/pages/Settings.module.scss';
 
 const Settings = (props) => {
-    return <div>Settings {props.message}</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.themesAndColors}>
+                <div className={styles.theme}>
+                    <h3 className={styles.themeText}>THEME</h3>
+                    <button className={styles.themeButton}></button>
+                </div>
+            <div className={styles.mode}>
+                <h3 className={styles.modeText}>MODE</h3>
+                <div className={styles.modeSlider}>
+                    <input type="checkbox" className={styles.modeCheckbox} />
+                </div>
+            </div>
+        </div>
+        <hr className={styles.line} />
+        </div>
+    );
 };
 
 export default Settings;
