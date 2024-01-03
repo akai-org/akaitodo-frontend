04a50814cb76fde5bd4391c67cff79393b439956
giro39 <127617431+giro39@users.noreply.github.com>
2024-01-03 20:56:36 +0100

    Add click handler

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,14 +1,29 @@
+import React, { useState } from 'react';
 import '#src/styles/App.scss';
 
 import styles from '#src/styles/pages/Settings.module.scss';
 
+
 const Settings = (props) => {
+    const [sidebar, setSidebar] = useState(false)
+
+    const sidebarStyle = {
+        display: sidebar ? "block" : "none"
+    }
+
+    const handleClick = () => {
+        setSidebar(prevState => {
+            // console.log(prevState)
+            return !prevState
+        })
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.themesAndColors}>
                 <div className={styles.theme}>
                     <h3 className={styles.themeText}>THEME</h3>
-                    <button className={styles.themeButton}></button>
+                    <button className={styles.themeButton} onClick={handleClick}></button>
                 </div>
             <div className={styles.mode}>
                 <h3 className={styles.modeText}>MODE</h3>
@@ -18,6 +33,7 @@ const Settings = (props) => {
             </div>
         </div>
         <hr className={styles.line} />
+        <p className={styles.test} style={sidebarStyle}>Test</p> 
         </div>
     );
 };
