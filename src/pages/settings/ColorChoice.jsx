7dcc3b4e8954893bfd611cf33629d31908ffa8c6
giro39 <127617431+giro39@users.noreply.github.com>
2024-01-06 17:49:40 +0100

    Fix colors, add file with colors

--- src/pages/settings/ColorChoice.jsx
+++ src/pages/settings/ColorChoice.jsx
@@ -1,37 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '#src/styles/App.scss';
-
+import colors from "./colors";
 import styles from '#src/styles/pages/settings/ColorChoice.module.scss';
-
-colors = {
-    red: ["#821c3f", "#be1951", "#e91e63", "#f5754c"],
-    pink: ["#6e193b", "#9f144a", "#c2185b", "#bb5534"],
-    violet: ["#5b2165", "#7a1f89", "#9c27b0", "#4f34bb"],
-    darkblue: ["#392165", "#4a2293", "#5727b0", "#5b2165"],
-    navy: ["#212265", "#222597", "#272ab0", "#392165"],
-    sea: ["#214365", "#235a93", "#276bb0", "#357e60"],
-    lightblue: ["#39637b", "#4a92ba", "#4a92ba", "#aaca2d"],
-    lightgreen: ["#397b6c", "#4dbaa1", "#57dcbe", "#2fa7cd"],
-    green: ["#3d7052", "#53a875", "#53a875", "#b7ba18"]
-}
 
 const ColorChoice = () => {
     return (
         <div className={styles.container}>
             <ColorButton />
-            <ColorButton />
-            <ColorButton />
-            <ColorButton />
-            <ColorButton />
         </div>
     )
 }
-
 
 const ColorButton = (props) => {
     // styleButton = {
     //     backgroundColor: props.color
     // }
+    // console.log(props)
     return (
         <div className={styles.firstLayer}>
             <div className={styles.secondLayer}>
---
+++ src/pages/settings/colors.jsx
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        name: "red",
+        color1: "#821c3f",
+        color2: "#be1951",
+        color3: "#e91e63",
+        color4: "#f5754c"
+    },
+    {
+        id: 2,
+        name: "pink",
+        color1: "#6e193b",
+        color2: "#9f144a",
+        color3: "#c2185b",
+        color4: "#bb5534"
+    },
+    {
+        id: 3,
+        name: "purple",
+        color1: "#5b2165",
+        color2: "#7a1f89",
+        color3: "#9c27b0",
+        color4: "#4f34bb"
+    },
+    {
+        id: 4,
+        name: "violet",
+        color1: "#392165",
+        color2: "#4a2293",
+        color3: "#5727b0",
+        color4: "#5b2165"
+    },
+    {
+        id: 5,
+        name: "navy",
+        color1: "#212265",
+        color2: "#222597",
+        color3: "#272ab0",
+        color4: "#392165"
+    },
+    {
+        id: 6,
+        name: "sea",
+        color1: "#214365",
+        color2: "#235a93",
+        color3: "#276bb0",
+        color4: "#357e60"
+    },
+    {
+        id: 7,
+        name: "lightblue",
+        color1: "#39637b",
+        color2: "#4a92ba",
+        color3: "#4a92ba",
+        color4: "#aaca2d"
+    },
+    {
+        id: 8,
+        name: "lightgreen",
+        color1: "#397b6c",
+        color2: "#4dbaa1",
+        color3: "#57dcbe",
+        color4: "#2fa7cd"
+    },
+    {
+        id: 9,
+        name: "green",
+        color1: "#3d7052",
+        color2: "#53a875",
+        color3: "#53a875",
+        color4: "#b7ba18"
+    }
+]
