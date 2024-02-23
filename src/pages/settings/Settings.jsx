cab2218bcb3e282356772a43e3fedf14677ec850
giro39 <127617431+giro39@users.noreply.github.com>
2024-02-23 12:00:51 +0100

    Add navbar styling

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,9 +1,42 @@
 import '#src/styles/App.scss';
+import { useLocation } from 'react-router-dom';
 
 import styles from '#src/styles/pages/Settings.module.scss';
+import NavbarModule from '../../layout/Navbar/NavbarModule';
+
+const availableFolds = [
+    "themes",
+    "calendar",
+    "todolist",
+    "notes",
+    "profile"
+];
+
+const navbarElementsArray = [
+    "Themes",
+    "Calendar",
+    "ToDo List",
+    "Notes",
+    "Profile"
+];
+
+const navbarElements = navbarElementsArray.map((element, index, array) => (
+    <div className={styles.elementWithPipe}>
+        <div className={styles.navbarElement}>{element}</div>
+        {index !== array.length - 1 && <div className={styles.navbarPipe}></div>}
+    </div>
+));
 
 const Settings = (props) => {
-    return <div>Settings {props.message}</div>;
+    return (
+        <div>
+            <NavbarModule>
+                <div className={styles.navbar}>
+                    {navbarElements}
+                </div>
+            </NavbarModule>
+        </div>
+    );
 };
 
 export default Settings;
