0083ceea0a9cf2606be713826a8bf5eec9a5ee02
Olej <music.olejniczak@wp.pl>
2024-03-11 01:11:27 +0100

    Initial search bar design

--- src/pages/notes/Notes.jsx
+++ src/pages/notes/Notes.jsx
@@ -1,13 +1,20 @@
 import styles from '#src/styles/pages/Notes.module.scss';
 import Note from '#src/components/notes/Note';
 import AddNote from '../../components/notes/AddNote';
+import NavbarModule from '../../layout/Navbar/NavbarModule';
+import SearchBar from '../../components/notes/SearchBar';
 
 const Notes = () => {
     return (
-        <div className={styles.container}>
-            <Note />
-            <AddNote />
-        </div>
+        <>
+            <NavbarModule>
+                <SearchBar />
+            </NavbarModule>
+            <div className={styles.container}>
+                <Note />
+                <AddNote />
+            </div>
+        </>
     );
 };
 
