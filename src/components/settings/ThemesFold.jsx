d4befc7ba2163c7d5dd2cc953524a769e886858e
giro39 <127617431+giro39@users.noreply.github.com>
2024-05-27 21:31:49 +0200

    Add themes functionality

--- src/components/settings/ThemesFold.jsx
+++ src/components/settings/ThemesFold.jsx
@@ -8,70 +8,70 @@ import { ThemeContext } from '../../contexts/ThemeContext';
 
 const colors = [
     {
-        name: "Default",
+        name: "default",
         primaryColor: "#222",
         secondaryColor: "#555",
         thirdColor: "#888",
         additionalColor: "#aaa",
     },
     {
-        name: "Red",
+        name: "red",
         primaryColor: "#821c3f",
         secondaryColor: "#be1951",
         thirdColor: "#e91e63",
         additionalColor: "#f5754c",
     },
     {
-        name: "Pink",
+        name: "pink",
         primaryColor: "#6e193b",
         secondaryColor: "#9f144a",
         thirdColor: "#c2185b",
         additionalColor: "#bb5534",
     },
     {
-        name: "Purple",
+        name: "purple",
         primaryColor: "#5b2165",
         secondaryColor: "#7a1f89",
         thirdColor: "#9c27b0",
         additionalColor: "#4f34bb",
     },
     {
-        name: "Violet",
+        name: "violet",
         primaryColor: "#392165",
         secondaryColor: "#4a2293",
         thirdColor: "#5727b0",
         additionalColor: "#5b2165",
     },
     {
-        name: "Navy",
+        name: "navy",
         primaryColor: "#212265",
         secondaryColor: "#222597",
         thirdColor: "#272ab0",
         additionalColor: "#392165",
     },
     {
-        name: "Sea",
+        name: "sea",
         primaryColor: "#214365",
         secondaryColor: "#235a93",
         thirdColor: "#276bb0",
         additionalColor: "#357e60",
     },
     {
-        name: "Lightblue",
+        name: "lightblue",
         primaryColor: "#39637b",
         secondaryColor: "#4a92ba",
         thirdColor: "#57acdc",
         additionalColor: "#aaca2d",
     },
     {
-        name: "Lightgreen",
+        name: "lightgreen",
         primaryColor: "#397b6c",
         secondaryColor: "#4dbaa1",
         thirdColor: "#57dcbe",
         additionalColor: "#2fa7cd",
     },
     {
-        name: "Green",
+        name: "green",
         primaryColor: "#3d7052",
         secondaryColor: "#53a875",
         thirdColor: "#60c689",
@@ -80,16 +80,35 @@ const colors = [
 ];
 
 
+const capitalizedFirstLetter = (name) => name.slice(0,1).toUpperCase() + name.slice(1);
+
+
 const ThemesFold = () => {
     const {theme, setTheme} = useContext(ThemeContext);
+    
+    const handleModeChange = () => {
+        setTheme(prevState => ({
+            ...prevState,
+            mode: prevState.mode === 'light' ? 'dark' : 'light'
+    }));
+    }
+
+    const handleStyleChange = (name) => {
+        setTheme(prevState => ({
+            ...prevState,
+            style: name
+        }))
+    }
+
 
     const colorPalettes = colors.map(palette => (
-        <div key={palette.name} className={styles.paletteElement}>
-            <p className={styles.paletteName}>{palette.name}</p>
+        <div key={palette.name} className={styles.paletteElement} onClick={() => handleStyleChange(palette.name)}>
+            <p className={styles.paletteName}>{capitalizedFirstLetter(palette.name)}</p>
             <div style={{backgroundColor: palette.additionalColor}} className={styles.firstLayer}>
                 <div style={{backgroundColor: palette.thirdColor}} className={styles.secondLayer}>
                     <div style={{backgroundColor: palette.secondaryColor}} className={styles.thirdLayer}>
                         <div style={{backgroundColor: palette.primaryColor}} className={styles.fourthLayer}>
+                            
                         </div>
                     </div>
                 </div>
@@ -101,7 +120,11 @@ const ThemesFold = () => {
         <div className={styles.container}>
             <div className={styles.modeContainer}>
                 <p className={styles.modeText}>MODE</p>
-                <input type="checkbox" style={{backgroundColor: 'tomato'}}/> {/* What's happening? */} 
+                <input 
+                    type="checkbox" 
+                    style={{opacity: '1', width: 'auto', height: 'auto'}}
+                    onChange={handleModeChange}
+                />
                 
             </div>
             <hr className={styles.line} />
